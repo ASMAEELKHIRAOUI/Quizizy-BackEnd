6d4491ca19380ff0b8f2caebd3d452552c990406
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2023-01-06 14:47:33 +0100

    refactured

--- DB/database.php
+++ DB/database.php
@@ -3,7 +3,6 @@
 class Database{
     static public function connection(){
       try {
-        //code...
          $db = new PDO("mysql:host=localhost;dbname=quizizyy","root","");
         $db ->exec("set names utf8");
         $db -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
--- pages/index.php
+++ pages/index.php
@@ -8,9 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
-    <!-- CSS only -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="../assets/sass/style.css"/>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <title>Quizizy</title>
@@ -37,7 +34,6 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="d-flex justify-content-end"></div> -->
         </div>
     </nav>
 <div id="home" class="home position-relative">
